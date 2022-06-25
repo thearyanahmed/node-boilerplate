6b018d225bc405fe966f4ad514389bfe113bf077
thearyanahmed <thearyanahmed@gmail.com>
2022-06-26 00:08:15 +0200

    Wip

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from "@nestjs/config"
-import { LoggerService, LogLevel } from "@nestjs/common"
-import { WINSTON_MODULE_NEST_PROVIDER, WinstonLogger } from "nest-winston"
+import { WINSTON_MODULE_NEST_PROVIDER} from "nest-winston"
 
 async function bootstrap() {
   // Create a new app instance
@@ -10,11 +9,6 @@ async function bootstrap() {
 
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
-  // const logger = await app.get(LoggerService)
-  // const logLevel : LogLevel = "log"
-  // logger.setLogLevels(logLevel)
-
-  // logger.log("this is a new logger")
   // Get the config service to read configuration values.
   const config_service = app.get<ConfigService>(ConfigService);
   const port = config_service.get<number>('port');
