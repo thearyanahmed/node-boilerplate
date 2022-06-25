f18ce48764c96687e5faaf1b89732f1caf7617b2
thearyanahmed <thearyanahmed@gmail.com>
2022-06-25 12:53:25 +0200

    Wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppLogger } from "./shared/logger/logger.service"
+import { SharedModule } from "./shared/shared.module"
 
 @Module({
   imports: [
-
+    SharedModule,
   ],
   controllers: [AppController],
   providers: [AppService, AppLogger],
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 import { ConfigService } from "@nestjs/config"
 
 async function bootstrap() {
@@ -7,9 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Get the config service to read configuration values.
-  const config_service = app.get(ConfigService)
-  const port = config_service.get<number>("port")
+  const config_service = app.get<ConfigService>(ConfigService);
+  const port = config_service.get<number>('port');
 
-  await app.listen(port);
+  await app.listen(3000);
 }
 bootstrap();
